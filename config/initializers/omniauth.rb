f6f107d4f3ea33ed49d5d6ef0b64b8b76789fd03
bisaiji08 <k.bisaiji@gmail.com>
2025-01-30 00:32:28 +0900

    googleアカウントを使ったログインができないバグを修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 OmniAuth.config.logger = Rails.logger
+OmniAuth.config.allowed_request_methods = %i[post get]
 
 OmniAuth.config.on_failure = proc do |env|
   message = env['omniauth.error.type']
@@ -8,5 +9,3 @@ OmniAuth.config.on_failure = proc do |env|
   Rails.logger.error "OmniAuth Error: #{strategy} - #{message}"
   [302, { 'Location' => "/auth/failure?message=#{message}" }, []]
 end
-
-OmniAuth.config.allowed_request_methods = %i[post get]
